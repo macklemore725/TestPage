65400c2aacf3b3724d99eb9e338013ba94da0163
macklemore725 <78071295+macklemore725@users.noreply.github.com>
2021-09-27 11:12:34 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+
+import App from "./App";
+
+
+// var genderlist = [{ Gender: "male" }, { Gender: "female" }, { Gender: "All" }];
+new Vue({
+  el: "#app",
+  render: (h) => h(App)});
+// });
+
+// function myFunction() {
+//   var final =   [{"Player": "Precious Achiuwa\\achiupr01"}]
+//   // for (var key in data) {
+//   //   final[key] = { Player: data["Player"] };
+//   // }
+
+//   //  console.log(final);
+//   // return final;
+//   return final;
+
+//   // console.log(vm.selected);// The function returns the product of p1 and p2
+// }
